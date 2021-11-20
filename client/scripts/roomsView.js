df4c8c681d499a00388d5c4a479e5ec7f1f3a978
David Rajec <david.rajec@gmail.com>
2021-11-19 21:27:40 -0800

    Added notes on how to proceed with the rest of the app.

--- client/scripts/roomsView.js
+++ client/scripts/roomsView.js
@@ -16,19 +16,24 @@ var RoomsView = {
 
   render: function() {
     // TODO: Render out the list of rooms.
+    // calls renderRoom for every room.
   },
 
   renderRoom: function(roomname) {
     // TODO: Render out a single room.
-
+    // In whatever way you display those rooms.
+    // It will append the room to the dropdown.
   },
 
   handleChange: function(event) {
     // TODO: Handle a user selecting a different room.
+    // jQuery way to isolate the currently-selected room.
+    // change messages that pop up given selected room.
   },
 
   handleClick: function(event) {
     // TODO: Handle the user clicking the "Add Room" button.
+    // handle adding a room (alert or input field)
   }
 
 };
